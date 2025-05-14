920cccb5b432cc3c0c581a9c6c96dd98bff6a459
ThingmasterGitHub <mjferriolo@gmail.com>
2025-05-14 13:21:01 -0400

    did a bunch of things

--- main.cpp
+++ main.cpp
@@ -4,33 +4,30 @@
 #include <limits>
 #include <SFML/Graphics.hpp>
 #include <bits/stdc++.h>
-unsigned int W = 600, H = 600, Wr = 6000, Hr = 6000;
-
+float W = 600, H = 600;
+int size=50;
+unsigned int frameCount = 0;
+float avg;
 std::vector<int> arr;
 class game{
     public:
 
         //runs program :3
         void run(){
-
             bool done=false;
-            fillVect(601);
+            fillVect(size);
 
             randomizeVect();
 
             //sayVect();
 
             sf::ContextSettings settings;
-            settings.antiAliasingLevel = 8.0;
-            sf::RenderWindow window(sf::VideoMode({W, H}), "sort"/*, sf::Style::default, settings*/);
+            settings.antiAliasingLevel = 16.0;
+            sf::RenderWindow window(sf::VideoMode({W, H}), "sort", sf::Style::Default,sf::State::Windowed,  settings);
 
+            //window.setFramerateLimit(240);
 
-
-            sf::RenderTexture RenderTexture({textS,textS});
-
-            RenderTexture.clear(sf::Color::White);
-
-            while (window.isOpen())
+            while(window.isOpen())
             {
 
                 while (const std::optional event = window.pollEvent())
@@ -44,24 +41,42 @@ class game{
                 }
 
 
+
                 if(check(window)){
+
+
 
                     done=true;
                 }
                 if(!done){
+                    //window.clear(sf::Color::Black);
+
                     window.clear(sf::Color::Black);
+
+                    backwardsSort();
 
 
                     displayBars(window);
 
                     window.display();
-
+                    frameCount++;
 
                     //sorting
-                    sort();
 
 
+
+                }else{
+                    avgArr();
+                    newList();
+                    window.clear(sf::Color::Black);
+                    displayBars(window);
+
+                    window.display();
+                    done=false;
+                    frameCount=0;
                 }
+
+
 
 
 
@@ -69,25 +84,45 @@ class game{
         }
 
     private:
+        void avgArr(){
+            avg=(avg*9+frameCount)/10;
+            std::cout<<avg<<" : "<<frameCount<<"\n";
+        }
+        void newList(){
+            size*=2;
+            //600
+            if(size>600){
+                size=600;
+            }
+            fillVect(size);
+            randomizeVect();
+
+        }
         bool check(sf::RenderWindow& window){
 
             float width, height;
             width =W/arr.size();
-            sf::RectangleShape shape(sf::Vector2f(width,height));
-            shape.setFillColor(sf::Color(0x00ff00ff));
 
 
 
             for(int i=0;i<arr.size();i++){
                 if(i==arr[i]){
 
+                    sf::RectangleShape rect(sf::Vector2f(width,height));
+                    rect.setFillColor(sf::Color(0x00ff00ff));
+                    sf::RectangleShape shape(sf::Vector2f(W,H));
                     height =(H/arr.size())*(arr[i]+1);
 
 
-                    shape.setSize(sf::Vector2f(width, height));
-                    shape.setPosition({width*i,W-height});
+                    rect.setSize(sf::Vector2f(width, height));
+                    rect.setPosition({width*i,H-height});
 
-                    window.draw(shape);
+                    window.draw(rect);
+
+
+                    window.display();
+
+
 
 
                     window.display();
@@ -120,23 +155,94 @@ class game{
             shuffle(arr.begin(), arr.end(), g);
         }
 
-        void displayBars(sf::RenderTarget& window){
+        void displayBars(sf::RenderWindow& window){
 
             float width, height;
             width =W/arr.size();
             for(auto i : arr){
                 height =(H/arr.size())*(arr[i]+1);
                 sf::RectangleShape shape(sf::Vector2f(width,height));
-                shape.setPosition({width*i,W-height});
+                shape.setPosition({width*i,H-height});
                 window.draw(shape);
             }
         }
 
-        void sort(){
+        void bublesort(){
             for(int i=1;i<arr.size();i++){
                 if(arr[i]<arr[i-1])
                 std::swap(arr[i],arr[i-1]);
             }
+        }
+        void backwardsSort(){
+            for(int i=arr.size()-1;i>0;i-=1){
+                if(arr[i]>arr[i+1])
+                    std::swap(arr[i],arr[i+1]);
+            }
+        }
+        //randomly moves index if value below if greater or value above is less
+        //switches to more reliable algorithm after 99.9 % of the time it does nothing
+        void sortRandom(){
+            float upCalc =0;
+
+            for(auto i : arr){
+                srand(time(0));
+                int iP = i+(rand() % (arr.size()-i));
+                int iL = arr.size()-iP;
+                //std::cout<<iP<<" : "<<iL<<" : "<<i<<"\n";
+                if(arr[i]>arr[iP]&&i<iP){
+                    std::swap(arr[i],arr[iP]);
+                }else if(arr[i]<arr[iL]&&i>iL){
+                    std::swap(arr[i],arr[iL]);
+                }else{
+                    upCalc++;
+                }
+
+            }
+            float perRight = upCalc/arr.size();
+
+            if(perRight>=0.999){
+                for(int i=1;i<arr.size();i++){
+                    if(arr[i]<arr[i-1])
+                        std::swap(arr[i],arr[i-1]);
+                }
+            }
+
+            std::cout<<perRight*100<<"%\n";
+
+
+        }
+        void sortLessRandom(){
+            float upCalc =0;
+
+            for(auto i : arr){
+                int iP,iL;
+                while(iP>arr.size()||iL<0){
+                    srand(time(0));
+                    iP = i+(rand() % 500);
+                    iL = i-(rand() % 500);
+                }
+                //std::cout<<iP<<" : "<<iL<<" : "<<i<<"\n";
+                if(arr[i]>arr[iP]&&i<iP){
+                    std::swap(arr[i],arr[iP]);
+                }else if(arr[i]<arr[iL]&&i>iL){
+                    std::swap(arr[i],arr[iL]);
+                }else{
+                    upCalc++;
+                }
+
+            }
+            float perRight = upCalc/arr.size();
+
+            if(perRight>=0.90){
+                for(int i=1;i<arr.size();i++){
+                    if(arr[i]<arr[i-1])
+                        std::swap(arr[i],arr[i-1]);
+                }
+            }
+
+            std::cout<<perRight*100<<"%\n";
+
+
         }
 
 };
